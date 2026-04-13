84c9d171a3dc7b7741f62a2b77b5483c6f8bebc1
abhinanthpy132 <abhinanth132@gmail.com>
2026-04-13 19:25:51 +0530

    Update vite.config.js main test upp pull

--- website/vite.config.js
+++ website/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reactwater2/', // Match your new repository name for GitHub Pages
+  base: '/t2/', // Match your new repository name for GitHub Pages
   build: {
     outDir: 'dist'
   }
